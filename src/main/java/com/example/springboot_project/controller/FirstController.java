2dde747ef7d3a42ffc699488390992cb1b803e61
JaehyeonPaak <wogus392@gmail.com>
2022-08-01 16:52:23 +0900

    Initial commit

---
+++ src/main/java/com/example/springboot_project/controller/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.springboot_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String greetings(Model model) {
+        model.addAttribute("username", "Jaehyeon");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String goodbye(Model model) {
+        model.addAttribute("username", "Jaehyeon");
+        return "goodbye";
+    }
+}
